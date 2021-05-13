cab64561f9be844faabd2a801592203b739258dc
CaoHao <1521930938@qq.com>
2021-05-13 20:37:45 +0800

    master

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="home">
+    <!-- 导航栏 -->
     <nav-bar class="home-nav">
       <div slot="center">ClothesMall</div>
     </nav-bar>
-    <h2>首页</h2>
-    <p>{{ recommends }}</p>
+    <!-- 轮播图 -->
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 <script>
 import NavBar from "../../components/common/navbar/NavBar.vue";
+import HomeSwiper from "./childComp/HomeSwiper.vue";
+
 import { getHomeMultidata } from "@/network/home";
+
 export default {
   name: "Home",
   components: {
     NavBar,
+    HomeSwiper,
   },
   created() {
     getHomeMultidata().then((res) => {
-      console.log(res);
       this.banners = res.data.banner.list;
       this.recommends = res.data.recommend.list;
     });
---
+++ src/views/home/childComp/HomeSwiper.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- 轮播图 -->
+  <swiper>
+    <!-- 注意！！！！！！！！！！！！！！！！！！ -->
+    <!-- 这里只能写成 v-for="(item, index) in banners" :key="index-->
+    <swiper-item v-for="(item, index) in banners" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+<script>
+import { Swiper, SwiperItem } from "@/components/common/swiper/index";
+export default {
+  name: "HomeSwiper",
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    Swiper,
+    SwiperItem,
+  },
+};
+</script>
+<style lang="">
+</style>
