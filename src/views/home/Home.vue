234c6c8e5823f2dbcde9fdd64795d2321b75b4a4
CaoHao <1521930938@qq.com>
2021-05-13 19:36:23 +0800

    master

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,14 +4,29 @@
       <div slot="center">ClothesMall</div>
     </nav-bar>
     <h2>首页</h2>
+    <p>{{ recommends }}</p>
   </div>
 </template>
 <script>
 import NavBar from "../../components/common/navbar/NavBar.vue";
+import { getHomeMultidata } from "@/network/home";
 export default {
   name: "Home",
   components: {
     NavBar,
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
   },
 };
 </script>
