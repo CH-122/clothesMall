dd68a37cfe9922c86ec8447430c473827d0d6abb
CaoHao <1521930938@qq.com>
2021-06-10 08:18:23 +0800

    分类页

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,148 +1,111 @@
 <template>
-  <div class="wrapper">
-    <ul class>
-      <button @click="btnClick">anniu</button>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-      <li>分类列表</li>
-    </ul>
+  <div id="category">
+    <nav-bar class="nav-bar"><div slot="center">商品分类</div></nav-bar>
+    <div class="content">
+      <tab-menu :categories="categories" @selectItem="selectItem"></tab-menu>
+
+      <scroll id="tab-content">
+        <div>
+          <tab-content-category
+            :subcategories="showSubcategory"
+          ></tab-content-category>
+          <!-- <tab-control :titles="['综合', '新品', '销量']"></tab-control> -->
+          <!-- <tab-content-detail></tab-content-detail> -->
+        </div>
+      </scroll>
+    </div>
   </div>
 </template>
 <script>
-import BScroll from "better-scroll";
+import Scroll from "../../components/common/scroll/Scroll";
+import NavBar from "../../components/common/navbar/NavBar.vue";
+import TabContentCategory from "./childComps/TabContentCategory.vue";
+import { getCategory, getSubcategory } from "../../network/category";
+import TabControl from "../../components/content/tabControl/TabControl.vue";
+import TabMenu from "./childComps/TabMenu.vue";
 export default {
+  components: { NavBar, TabContentCategory, TabControl, TabMenu, Scroll },
   data() {
     return {
+      categories: [],
       scroll: null,
       probeType: 0,
+      currentIndex: -1,
+      categoryData: {},
     };
   },
-  mounted() {
-    // 使用BScrool创建滚动
-    this.scroll = new BScroll(".wrapper", {
-      probeType: 0,
-      pullUpLoad: true,
-      click: false,
-    });
-
-    this.scroll.on("scroll", (position) => {
-      // console.log(position);
-    });
-
-    this.scroll.on("pullingUp", () => {
-      setTimeout(() => {
-        console.log("上拉加载更多");
-      }, 2000);
-    });
-  },
-  methods: {
-    btnClick() {
-      console.log("btnClick");
-      console.log(this.probeType);
+  computed: {
+    showSubcategory() {
+      if (this.currentIndex === -1) return {};
+      console.log(this.categoryData[this.currentIndex].subcategories);
+      return this.categoryData[this.currentIndex].subcategories;
     },
+  },
+  mounted() {},
+  methods: {
+    /* 
+    网络请求方法
+    */
+    getCategory() {
+      getCategory().then((res) => {
+        console.log(res);
+        this.categories = res.data.category.list;
+        // 2.初始化每个类别的子数据
+        for (let i = 0; i < this.categories.length; i++) {
+          this.categoryData[i] = {
+            subcategories: {},
+          };
+        }
+        // 3.请求第一个分类的数据
+        this.getSubcategories(0);
+      });
+    },
+
+    getSubcategories(index) {
+      this.currentIndex = index;
+      const maitKey = this.categories[this.currentIndex].maitKey;
+      getSubcategory(maitKey).then((res) => {
+        this.categoryData[index].subcategories = res.data;
+        this.categoryData = { ...this.categoryData };
+      });
+    },
+
+    /* 
+      事件方法
+    */
+    selectItem(index) {
+      this.getSubcategories(index);
+    },
+  },
+  created() {
+    this.getCategory();
   },
 };
 </script>
 
 <style scoped>
-.wrapper {
-  height: 300px;
-  background-color: powderblue;
-  /* overflow: scroll; */
+#category {
+  height: 100vh;
+}
+
+.nav-bar {
+  background-color: rgb(130, 116, 236);
+  color: #fff;
   overflow: hidden;
+}
+
+.content {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 44px;
+  bottom: 49px;
+  overflow: hidden;
+  display: flex;
+}
+
+#tab-content {
+  height: 100%;
+  flex: 1;
 }
 </style>
---
+++ src/views/category/childComps/TabContentCategory.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <grid-view>
+      <div class="item">
+        <a v-for="(item, index) in subcategories.list" :key="index">
+          <img class="item-img" :src="item.image" alt="" />
+          <div class="item-text">{{ item.title }}</div>
+        </a>
+      </div>
+    </grid-view>
+  </div>
+</template>
+
+<script>
+import GridView from "../../../components/common/gridView/GridView.vue";
+
+export default {
+  name: "TabContentCategory",
+  components: {
+    GridView,
+  },
+  props: {
+    subcategories: {
+      type: Object,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.item {
+  width: 81px;
+  text-align: center;
+  font-size: 12px;
+}
+
+.item-img {
+  width: 50%;
+  margin-top: 8px;
+}
+
+.item-text {
+  margin-top: 8px;
+}
+</style>
